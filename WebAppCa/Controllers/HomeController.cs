62d8ef2f1c662c618747c404325c79237a7ed355
Carl Björklund <carl.bjorklund@outlook.com>
2018-11-07 17:33:42 +0100

    minorupdates

--- WebAppCa/Controllers/HomeController.cs
+++ WebAppCa/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebAppCa.ViewModels;
+using WebAppCa.Models;
 
 namespace WebAppCa.Controllers
 {
