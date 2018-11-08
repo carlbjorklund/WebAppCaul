fe5a50de9c955352a9efd0dcd619112415c2b398
Carl Björklund <carl.bjorklund@outlook.com>
2018-11-08 07:12:53 +0100

    login and user

--- WebAppCa/Controllers/AccountsController.cs
+++ WebAppCa/Controllers/AccountsController.cs
@@ -8,49 +8,75 @@ using WebAppCa.ViewModels;
 
 namespace WebAppCa.Controllers
 {
-    public class AccountController : Controller
-    {
-        UserManager<IdentityUser> userManager;
-
-        public AccountController(UserManager<IdentityUser> _userManager)
+  public class AccountController : Controller
         {
-            userManager = _userManager;
-        }
+            UserManager<IdentityUser> userManager;
+            SignInManager<IdentityUser> signInManager;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            IdentityUser user = new IdentityUser()
+            public AccountController(UserManager<IdentityUser> _userManager, SignInManager<IdentityUser> _signInManager)
             {
-                UserName = model.UserName,
-                Email = model.Email
-            };
-
-            var result = await userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Login", "Account");
+                userManager = _userManager;
+                signInManager = _signInManager;
             }
 
-            return View();
-        }
+            public IActionResult Index()
+            {
+                return View();
+            }
 
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
+            [HttpGet]
+            public IActionResult Register()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Register(RegisterViewModel model)
+            {
+                IdentityUser user = new IdentityUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                string confirmationToken = await userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                string confirmationLink = Url.Action("ConfirmEmail", "Account", new { userid = user.Id, token = confirmationToken }, protocol: HttpContext.Request.Scheme);
+
+                System.IO.File.WriteAllText(@"C:\Temp\ConfirmEmail.txt", confirmationLink);
+
+                //Send Emal ----
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+
+                return View();
+            }
+
+            [HttpGet]
+            public IActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Login(LoginViewModel model)
+            {
+                var Result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, true);
+                if (Result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Logout()
+            {
+                await signInManager.SignOutAsync();
+                return RedirectToAction("Login", "Account");
+            }
         }
     }
-}
