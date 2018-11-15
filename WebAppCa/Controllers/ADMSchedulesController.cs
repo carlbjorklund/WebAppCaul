fbcb2dfc3aa989dc76f41a31b32d5c5c6ed275c7
Carl Björklund <carl.bjorklund@outlook.com>
2018-11-15 17:08:47 +0100

    admin layout

---
+++ WebAppCa/Controllers/ADMSchedulesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppCa.Models;
+
+namespace WebAppCa.Controllers
+{
+    public class ADMSchedulesController : Controller
+    {
+        private readonly BroadCastContext _context;
+
+        public ADMSchedulesController(BroadCastContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ADMSchedules
+        public async Task<IActionResult> Index()
+        {
+            var broadCastContext = _context.Schedules.Include(s => s.Channel).Include(s => s.Programme);
+            return View(await broadCastContext.ToListAsync());
+        }
+
+        // GET: ADMSchedules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Channel)
+                .Include(s => s.Programme)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // GET: ADMSchedules/Create
+        public IActionResult Create()
+        {
+            ViewData["ChannelId"] = new SelectList(_context.Channels, "ChannelId", "ChannelId");
+            ViewData["ProgrammeId"] = new SelectList(_context.Programmes, "ProgrammeId", "ProgrammeId");
+            return View();
+        }
+
+        // POST: ADMSchedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ScheduleId,Image,AirDate,StartTime,EndTime,Sorting,ChannelId,ProgrammeId")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChannelId"] = new SelectList(_context.Channels, "ChannelId", "ChannelId", schedule.ChannelId);
+            ViewData["ProgrammeId"] = new SelectList(_context.Programmes, "ProgrammeId", "ProgrammeId", schedule.ProgrammeId);
+            return View(schedule);
+        }
+
+        // GET: ADMSchedules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            ViewData["ChannelId"] = new SelectList(_context.Channels, "ChannelId", "ChannelId", schedule.ChannelId);
+            ViewData["ProgrammeId"] = new SelectList(_context.Programmes, "ProgrammeId", "ProgrammeId", schedule.ProgrammeId);
+            return View(schedule);
+        }
+
+        // POST: ADMSchedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ScheduleId,Image,AirDate,StartTime,EndTime,Sorting,ChannelId,ProgrammeId")] Schedule schedule)
+        {
+            if (id != schedule.ScheduleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(schedule.ScheduleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChannelId"] = new SelectList(_context.Channels, "ChannelId", "ChannelId", schedule.ChannelId);
+            ViewData["ProgrammeId"] = new SelectList(_context.Programmes, "ProgrammeId", "ProgrammeId", schedule.ProgrammeId);
+            return View(schedule);
+        }
+
+        // GET: ADMSchedules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Channel)
+                .Include(s => s.Programme)
+                .FirstOrDefaultAsync(m => m.ScheduleId == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // POST: ADMSchedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var schedule = await _context.Schedules.FindAsync(id);
+            _context.Schedules.Remove(schedule);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return _context.Schedules.Any(e => e.ScheduleId == id);
+        }
+    }
+}
--- WebAppCa/Controllers/AdminProgrammesController.cs
+++ WebAppCa/Controllers/AdminProgrammesController.cs
@@ -6,69 +6,28 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.ResultOperators.Internal;
 using WebAppCa.Models;
 
 namespace WebAppCa.Controllers
 {
-    public class ProgrammesController : Controller
+    [Authorize(Roles = "Admin")]
+    public class AdminProgrammesController : Controller
     {
         private readonly BroadCastContext _context;
 
-        public ProgrammesController(BroadCastContext context)
+        public AdminProgrammesController(BroadCastContext context)
         {
             _context = context;
         }
 
-        //GET: Programmes
-       
-        /// <summary>
-        /// funkar
-        /// </summary>
-        /// <returns></returns>
-        //public async Task<IActionResult> Index()
-        //{
-
-
-        //    var broadCastContext = _context.Programmes.Include(p => p.Category);
-        //    return View(await broadCastContext.ToListAsync());
-        //}
-
-        public async Task<IActionResult> Index(int? CategoryId)
+        // GET: AdminProgrammes
+        public async Task<IActionResult> Index()
         {
-            var categories = _context.Categories.OrderBy(q => q.Title).ToList();
-            ViewData["CategoryId"] = new SelectList(categories, "CategoryId", "Title", CategoryId);
-
-            var broadCastContext = _context.Programmes
-                .Where(c => !CategoryId.HasValue || c.CategoryId == CategoryId)
-                .OrderBy(d => d.CategoryId)
-                .Include(d => d.Category);
+            var broadCastContext = _context.Programmes.Include(p => p.Category);
             return View(await broadCastContext.ToListAsync());
         }
 
-        /// <summary>
-        /// funkar spara
-        /// </summary>
-        /// <param name="CategoryId"></param>
-        /// <returns></returns>
-        public ActionResult GetCategories(int? CategoryId)
-        {
-            //ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Title");
-            var categories = _context.Categories.OrderBy(q => q.Title).ToList();
-            ViewData["CategoryId"] = new SelectList(categories, "CategoryId", "Title", CategoryId);
-            int categoryId = CategoryId.GetValueOrDefault();
-
-            var programmes = _context.Programmes
-                .Where(c => !CategoryId.HasValue || c.CategoryId == categoryId)
-                .OrderBy(d => d.CategoryId)
-                .Include(d => d.Category);
-            
-            return View(programmes.ToList());
-        }
-
-
-
-        // GET: Programmes/Details/5
+        // GET: AdminProgrammes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -77,12 +36,8 @@ namespace WebAppCa.Controllers
             }
 
             var programme = await _context.Programmes
-                .OrderBy(d => d.CategoryId)
                 .Include(p => p.Category)
-            .FirstOrDefaultAsync(m => m.ProgrammeId == id);
-
-            //.Include(d => d.Category)
-            //.FirstOrDefaultAsync(m => m.ProgrammeId == id);
+                .FirstOrDefaultAsync(m => m.ProgrammeId == id);
             if (programme == null)
             {
                 return NotFound();
@@ -91,18 +46,16 @@ namespace WebAppCa.Controllers
             return View(programme);
         }
 
-        // GET: Programmes/Create
-        [Authorize(Roles = "Admin")]
+        // GET: AdminProgrammes/Create
         public IActionResult Create()
         {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Title");
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
             return View();
         }
 
-        // POST: Programmes/Create
+        // POST: AdminProgrammes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ProgrammeId,Title,Description,Image,Length,CategoryId")] Programme programme)
@@ -117,8 +70,7 @@ namespace WebAppCa.Controllers
             return View(programme);
         }
 
-        // GET: Programmes/Edit/5
-        [Authorize(Roles = "Admin")]
+        // GET: AdminProgrammes/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -135,11 +87,9 @@ namespace WebAppCa.Controllers
             return View(programme);
         }
 
-
-        // POST: Programmes/Edit/5
+        // POST: AdminProgrammes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ProgrammeId,Title,Description,Image,Length,CategoryId")] Programme programme)
@@ -173,8 +123,7 @@ namespace WebAppCa.Controllers
             return View(programme);
         }
 
-        // GET: Programmes/Delete/5
-        [Authorize(Roles = "Admin")]
+        // GET: AdminProgrammes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -193,7 +142,7 @@ namespace WebAppCa.Controllers
             return View(programme);
         }
 
-        // POST: Programmes/Delete/5
+        // POST: AdminProgrammes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
